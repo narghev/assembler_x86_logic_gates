577a9e30df091a96577a01542f57b9083100ce8b
Narek Ghevandiani <n.ghevandiani@gmail.com>
2017-12-07 01:33:11 +0400

    WIP sqrt

--- sqrt.asm
+++ sqrt.asm
@@ -1,6 +1,6 @@
 ; SQUARE ROOT OF A NUMBER
 ; Newton's algorithm
-; new_appx = (old_appx + (N/old_appx))/2
+; new_appx = (old_appx + (N/old_appx))
 ; continue doing untill new_appx == old_appx
 
 .model small
@@ -16,8 +16,32 @@
         mov ax, @data
         mov ds, ax ; set ds to be a pointer to data segment
         
-        mov ah, 02h ; 02h is for printing chars
-        mov dx, stid
-        int 21h ; prints 'd' on the screen, ascii code - 64h
+    sqrt:
+        mov bx, 01h
+        
+    sqrtloop:
+        
+        mov dx, bx ; dx becomes the old apprx
+        mov cx, stid ; keep N in cx
+        
+        mov ax, cx
+        div bx ; divide N by old_appx
+        mov cx, ax
+        
+        add bx, cx ; (old_appx + (N/old_appx))
+        
+        mov ax, bx
+        mov cx, 02h
+        div cx ; (old_appx + (N/old_appx))/2
+        mov bx, ax
+        
+        cmp dx, bx ; compare old appx with new appx
+        je result ; if equal go to result
+        jmp sqrtloop ; else loop again
 
+    result:
+        mov ah, 02h
+        mov dx, bx
+        int 21h
+        
         end start
