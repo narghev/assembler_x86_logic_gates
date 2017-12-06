bddc5566cfe5e31a7cfedd35cff294a2ae234d13
Narek Ghevandiani <n.ghevandiani@gmail.com>
2017-12-07 00:48:55 +0400

    pritig char on the screen

--- sqrt.asm
+++ sqrt.asm
@@ -1,29 +1,23 @@
 ; SQUARE ROOT OF A NUMBER
-MODEL SMALL
-    .STACK 100
-    .DATA
-        STID DW 464H ; Initialize num1 to 0019
-        SQRT DW 01 DUP (?) ; Reserve 1 word of uninitialised data space to offset sqrt
-    .CODE ; Code segment starts
-        START:
-            MOV AX, @DATA ;Initialize data segment
-            MOV DS, AX
-            MOV AX, 0001H ;Initialize BX to 0001H
-            MOV CX, 0001H ;Initialize CX to 0001H
-        LOOP1:
-            LEA DX, AX		; that the value entered
-            INT 21H		; is a prime by a DOS console out of NL2 ASCII string
-            CMP STID, AX
-            JG RESULT
-            INC CX
-            MUL AX
+; Newton's algorithm
+; new_appx = (old_appx + (N/old_appx))/2
+; continue doing untill new_appx == old_appx
 
+.model small
 
+.stack 100h
 
-            ;SUB AX, BX ;AX=AX-BX
-            ;JZ LOOP2 ; If zero flag is zero jump to loop2
-            ;INC CX ; Increment CX by 1
-            ;ADD BX, 0002H ;BX=BX+0002H
-            ;JMP LOOP1 ; Jump to loop1
-        RESULT:
-            END START
+.data
+    stid dw 0464H 
+
+.code
+    start:
+
+        mov ax, @data
+        mov ds, ax ; set ds to be a pointer to data segment
+        
+        mov ah, 02h ; 02h is for printing chars
+        mov dx, stid
+        int 21h ; prints 'd' on the screen, ascii code - 64h
+
+        end start
