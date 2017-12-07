9fbc70d28d906e01203c9bf2aca8e833e2d90abc
Narek Ghevandiani <n.ghevandiani@gmail.com>
2017-12-07 14:20:07 +0400

    comments

--- sqrt.asm
+++ sqrt.asm
@@ -10,17 +10,17 @@
     main:
 
         mov ax, @data
-        mov ds, ax ; set ds to be a pointer to data segment
+        mov ds, ax      ; set ds to be a pointer to data segment
 
         stid equ 464h
-        mov bx, 1 ; bx is the old apprx, set it to 1
+        mov bx, 1       ; bx is the old apprx, set it to 1
         
     sqrtloop:
-        mov dx, 0
-        mov ax, stid ; keep N in ax
-        div bx ; (N/old_appx)
-        add ax, bx ; (old_appx + (N/old_appx))
-        shr ax, 1 ; divide by 2
+        mov dx, 0       ; reset the remainder
+        mov ax, stid    ; keep N in ax
+        div bx          ; (N/old_appx)
+        add ax, bx      ; (old_appx + (N/old_appx))
+        shr ax, 1       ; divide by 2
         
         ;cmp dx, bx ; compare old appx with new appx
         ;je result ; if equal go to result
@@ -32,7 +32,7 @@
         mov ah, 02h
         int 21h
 
-        mov ah, 4ch	; setup to terminate program and
-        int 21h	; return to the DOC prompt
+        mov ah, 4ch	    ; setup to terminate program and
+        int 21h	        ; return to the DOC prompt
         
         end main
