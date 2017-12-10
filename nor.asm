24cbac36cd163d802a82749becc25e54fd630610
Narek Ghevandiani <n.ghevandiani@gmail.com>
2017-12-10 16:35:21 +0400

    or

--- nor.asm
+++ nor.asm
@@ -3,7 +3,7 @@
 .data
 .code
 
-    nor:
+    _nor:
         mov bl, al          ; copy of input bits into BL
         mov cl, al          ; and another in CL
         and bl, 00000001b   ; mask off all bits except input bit 0
--- or.asm
+++ or.asm
@@ -3,15 +3,14 @@
 .data
 .code
 
-    _nand:
+    _or:
         mov bl, al          ; copy of input bits into BL
         mov cl, al          ; and another in CL
         and bl, 00000001b   ; mask off all bits except input bit 0
         and cl, 00000010b   ; mask off all bits except input bit 1
         shr cl, 1           ; move bit 1 value into bit 0 of CL register
         
-        and bl, cl          ; AND these two registers, result in BL
-        not bl              ; invert bits for the not part of nand
+        or bl, cl           ; OR these two registers, result in BL
         and bl, 00000001b   ; clear all upper bits positions leaving bit 0 either a zero or one 
         
         mov ah, bl          ; copy answer into return value register
